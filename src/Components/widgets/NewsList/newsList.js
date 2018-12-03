fdaa3cbb7534b8557b8a9300a8307593c9436c78
monirul89 <monirul89@gmail.com>
2018-12-03 17:50:29 +0600

    no message

--- src/Components/widgets/NewsList/newsList.js
+++ src/Components/widgets/NewsList/newsList.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 import './newsList.css';
@@ -17,37 +17,46 @@ class NewsList extends Component{
         this.request(this.state.start, this.state.end)
     }
     request = (start, end)=>{
-        axios.get('http://localhost:3004/articles?_start='+ start +'&_end='+ end)        
+        axios.get('http://localhost:3004/articles?_start='+start+'&_end='+end)        
         .then( Response => {
             this.setState({
                 items:[...this.state.items, ...Response.data]
             })
         })
     }
-        const renderNews = (type) =>{
-            let template = null;
+    
+    renderNews =  (type)=>{
 
-            switch(type){
-                case ('card'):
-                    template =  this.state.map( (item, i)=>(
-                        <div>
-                            <div className="newsList_item">
-                                <Link to={'/artticles/'+ item.id}>
-                                    <h2>{ item.titlle }</h2>
-                                </Link>
-                            </div>
-                        </div>
-                    ))
-                    break;
-                default:
-                    template=null;
-            }
-            return template;
+        let template = null;
+
+        switch(type){
+            case ('card'):
+                template =  this.state.items.map( (item, i)=>(
+                    <div className="newsList_item">
+                        <Link to={'/artticles/'+ item.id}>
+                            <h2>{ item.title }</h2>
+                        </Link>
+                    </div>
+                ))
+                break;
+            default:
+                template=null;
         }
+        return template;
+    }
+            
+            
+
         render(){
+            console.log(this.state.items)
             return(
-                <div>
-                    { this.renderNews( this.props.type )}
+                <div>                    
+                    <div className="listArea">
+                        { this.renderNews( this.props.type)}
+                        <div>
+                        LOAD MORE
+                        </div>                    
+                    </div>
                 </div>
             )
         }
